af9f7c4e1d52683635d47fa797005e194c7a2cb8
M1vosh <103107282+M1vosh@users.noreply.github.com>
2023-12-22 20:05:07 +0100

    Front End User Authentication
    added user login reducer & action & screen & functionality,
    added username in Navbar & logout function,
    added user register reducer & action & screen,
    made profile update endpoint,
    added profile screen & get user details & updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
           <Routes>
             {" "}
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
             <Route path="/cart" element={<CartScreen />} />
